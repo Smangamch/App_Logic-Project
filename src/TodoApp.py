a187a4c7dfc3b3029add8bfba965b18e73dd1aa3
Smangamch <113479264+Smangamch@users.noreply.github.com>
2024-07-13 11:28:03 +0200

    Add files via upload

--- src/TodoApp.py
+++ src/TodoApp.py
@@ -11,8 +11,6 @@ class Todo_Items:
         self.created_on = datetime.now()
         self.completed_on = datetime.now()
         
-    
-
 
 class TodoApp:
     def __init__(self):
@@ -28,7 +26,7 @@ class TodoApp:
     
     # 2. Method to update and make changes to the items
     async def update(self, todo_id: int, description: str): # The update method checks the todo dictionary to see if the todo_id to check if it exists within 
-        for todo_item in self.todo:                         # the data structure and then is going to modify the description of the retrieved TodoItem
+                                                            # the data structure and then is going to modify the description of the retrieved TodoItem
             if todo_item.id == todo_id:
                 todo_item = self.todo[todo_id]
                 todo_item.description = description
@@ -38,15 +36,13 @@ class TodoApp:
     
     # 3. Method to delete a todo item
     async def delete(self, todo_id: int) -> None:       # This delete method checks to see if todo dictionary has an item and the deletes if found
-        
         if todo_id not in self.todo:
             return f"Todo item with ID {todo_id} not found!"
         del self.todo[todo_id]
         
     # 4. Method to set the todo  item as being complete
     async def complete(self, todo_id: int) -> bool:
-        for todo_item in self.todo:
-            if todo_item.id == todo_id:
+            if Todo_Items.id == todo_id:
                 self.todo[todo_id].complete()
                 return True
             else:
